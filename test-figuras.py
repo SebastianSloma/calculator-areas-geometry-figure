d1449ef88a3f808deecf528f89bd080e9cbb3af1
Sebastian <sebastiansloma@gmail.com>
2022-11-23 09:20:49 +0100

    added IntEnum library

--- test-figuras.py
+++ test-figuras.py
@@ -1,32 +1,36 @@
 import areas
 
-choice = input("""Wybierz figure której pole chcesz obliczyć:
+from enum import IntEnum
+
+Menu_Figures = IntEnum('Menu_Figuers', 'Square Ractangle Circle Triangle Trapeze')
+
+choice = int(input("""Wybierz figure której pole chcesz obliczyć:
 1. Kwadrat
 2. Prostokąt
 3. Koło
-4.Trójkąt
+4. Trójkąt
 5. Trapez
-""")
+"""))
 
-if(choice == '1'):
+if(choice == Menu_Figures.Square):
     a = int(input("Podaj bok kwadratu: "))
     print("Pole kwadratu wynosi: ", areas.area_square(a))
 
-if(choice == '2'):
+elif(choice == Menu_Figures.Ractangle):
     a = int(input("Podaj pierwszy bok prostokątu: "))
     b = int(input("Podaj drugi bok prostokątu: "))
     print("Pole prostokątu wynosi: ", areas.area_rectangle(a, b))
 
-if(choice == '3'):
+elif(choice == Menu_Figures.Circle):
     r = int(input("Podaj promień koła: "))
     print("Pole koła wynosi: ", areas.area_circle(r))
 
-if(choice == '4'):
+elif(choice == Menu_Figures.Triangle):
     a = int(input("Podaj bok trójkąta: "))
     h = int(input("Podaj wysokość trójkąta: "))
     print("Pole trójkąta wynosi: ", areas.area_triangle(a, h))
 
-if(choice == '5'):
+elif(choice == Menu_Figures.Trapeze):
     a = int(input("Podaj pierwszy bok trapezu: "))
     b = int(input("Podaj drugi bok trapezu: "))
     h = int(input("Podaj wysokość trapezu: "))
