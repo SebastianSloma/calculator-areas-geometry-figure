59966c02ec474098950331e528e2f24a4f3b56a3
Sebastian <sebastiansloma@gmail.com>
2022-11-23 08:48:55 +0100

    lil corect

--- test-figuras.py
+++ test-figuras.py
@@ -1,6 +1,6 @@
 import areas
 
-Wybór = input("""Wybierz figure której pole chcesz obliczyć:
+choice = input("""Wybierz figure której pole chcesz obliczyć:
 1. Kwadrat
 2. Prostokąt
 3. Koło
@@ -8,25 +8,25 @@ Wybór = input("""Wybierz figure której pole chcesz obliczyć:
 5. Trapez
 """)
 
-if(Wybór == '1'):
+if(choice == '1'):
     a = int(input("Podaj bok kwadratu: "))
     print("Pole kwadratu wynosi: ", areas.area_square(a))
 
-if(Wybór == '2'):
+if(choice == '2'):
     a = int(input("Podaj pierwszy bok prostokątu: "))
     b = int(input("Podaj drugi bok prostokątu: "))
     print("Pole prostokątu wynosi: ", areas.area_rectangle(a, b))
 
-if(Wybór == '3'):
+if(choice == '3'):
     r = int(input("Podaj promień koła: "))
     print("Pole koła wynosi: ", areas.area_circle(r))
 
-if(Wybór == '4'):
+if(choice == '4'):
     a = int(input("Podaj bok trójkąta: "))
     h = int(input("Podaj wysokość trójkąta: "))
     print("Pole trójkąta wynosi: ", areas.area_triangle(a, h))
 
-if(Wybór == '5'):
+if(choice == '5'):
     a = int(input("Podaj pierwszy bok trapezu: "))
     b = int(input("Podaj drugi bok trapezu: "))
     h = int(input("Podaj wysokość trapezu: "))
